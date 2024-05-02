16b1e836d23f56f89c918c59995b586ff21073c0
BhuvaneshMarineni <122952070+BhuvaneshMarineni@users.noreply.github.com>
2024-05-01 22:38:34 -0400

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-// Background script to handle message passing
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("Received message in background script:", request);
-    if (request.action === "logWordTime") {
-        console.log("Word:", request.word, "Time:", request.time);
-    }
-});
